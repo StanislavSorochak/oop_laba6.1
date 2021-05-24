2f3499ca16b64d5ab7d9b6d8c195eab5467198fc
StanislavSorochak <75738611+StanislavSorochak@users.noreply.github.com>
2021-05-24 11:11:01 +0300

    first master commit

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,24 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../oop_laba6.1/Array.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			Array unitTest = Array(20);
+
+			for (int i = 0; i < 20; i++)
+				unitTest[i] = 0;
+			unitTest[10] = -5;
+
+			Assert::AreEqual(unitTest.minValue(), -5.);
+		}
+	};
+}
